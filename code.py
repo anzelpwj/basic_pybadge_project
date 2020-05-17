09f8412190b09e68b59465e0415659227e8c549f
Paul Anzel <anzel.paul@heb.com>
2020-05-17 12:00:33 -0500

    Add flashing lights

--- code.py
+++ code.py
@@ -1,11 +1,15 @@
 from adafruit_pybadger import pybadger
 import time
 
+BLACK = (0, 0, 0)
+COLOR1 = (255, 0, 0)
+COLOR2 = (255, 255, 0)
+COLOR3 = (0, 255, 0)
+COLOR4 = (0, 255, 255)
+COLOR5 = (0, 0, 255)
+
 class State:
     time_length = 50  # Tens of seconds
-
-    def __init__(self):
-        pass
 
     def run(self):
         pass
@@ -40,6 +44,31 @@ prev_b = False
 prev_start = False
 timer = 50
 
+print('first')
+
+print('second')
+
+def pixel_display(timer, max_timer):
+    # Times look backwards due to timer decrementing
+    time5 = timer < int(max_timer/5)
+    time4 = int(max_timer/5) <= timer < int(2*max_timer/5)
+    time3 = int(2*max_timer/5) <= timer < int(3*max_timer/5)
+    time2 = int(3*max_timer/5) <= timer < int(4*max_timer/5)
+    time1 = int(4*max_timer/5) <= timer
+    clr1 = COLOR1 if time1 else BLACK
+    clr2 = COLOR2 if time2 else BLACK
+    clr3 = COLOR3 if time3 else BLACK
+    clr4 = COLOR4 if time4 else BLACK
+    clr5 = COLOR5 if time5 else BLACK
+
+    pybadger.pixels[0] = clr1
+    pybadger.pixels[1] = clr2
+    pybadger.pixels[2] = clr3
+    pybadger.pixels[3] = clr4
+    pybadger.pixels[4] = clr5
+    pybadger.pixels.brightness = 0.1
+
+
 def change_state(state):
     state %= len(states)
     states[state].run()
@@ -49,8 +78,10 @@ def change_state(state):
 
 state, timer, change_timer = change_state(state)
 
+#Main loop
 while True:
     state_change = False
+    pixel_display(timer, states[state].time_length)
     cur_a = pybadger.button.a
     cur_b = pybadger.button.b
     cur_start = pybadger.button.start
